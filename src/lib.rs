42ab187742e8f93cfd8a0e93c50a097c01966ecd
Patrik Simic <patrik.simic@gmail.com>
2021-04-20 20:24:11 +0200

    cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -5,18 +5,6 @@ use wasm_bindgen::prelude::wasm_bindgen;
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
-extern "C" {
-    #[wasm_bindgen(js_namespace = console)]
-    fn log(s: &str);
-
-    #[wasm_bindgen(js_namespace = console, js_name = log)]
-    fn log_u32(a: u32);
-
-    #[wasm_bindgen(js_namespace = console, js_name = log)]
-    fn log_many(a: &str, b: &str);
-}
-
-#[wasm_bindgen]
 pub fn compute(rounds: u32) -> u32 {
     let mut count: u32 = 0;
     for _ in 0..rounds {
@@ -24,7 +12,6 @@ pub fn compute(rounds: u32) -> u32 {
             count += 1;
         }
     }
-    log_u32(count);
     count
 }
 
@@ -37,23 +24,4 @@ pub fn say() -> String {
 #[wasm_bindgen]
 pub fn echo(content: &str) -> String {
     return content.to_string();
-}
-
-
-#[wasm_bindgen]
-pub fn test_log() {
-    log("Testing console log from Rust!");
-    log_u32(42);
-    log_many("Logging", "many values!");
-    using_a_macro();
-}
-
-macro_rules! console_log {
-    ($($t:tt)*) => (log(&format_args!($($t)*).to_string()))
-}
-
-fn using_a_macro() {
-    console_log!("Hello {}!", "rust macros");
-    console_log!("Let's print some numbers...");
-    console_log!("1 + 3 = {}", 1 + 3);
 }
--- test.ts
+++ test.ts
@@ -1,13 +1,12 @@
-import init , { source, compute, say, echo, test_log } from "./wasm.js";
+import init , { source, compute, say, echo } from "./wasm.js";
 
 await init(source);
 
 const testCompute = compute(42);
+console.log(testCompute);
 
-const testSay = say()
-console.log(testSay)
+const testSay = say();
+console.log(testSay);
 
 const testEcho = echo("Hola Rust!!!");
 console.log(testEcho);
-
-test_log();
