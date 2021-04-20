e17e9dd0cbcecbd825c6ddea34f9792441345304
Patrik Simic <patrik.simic@gmail.com>
2021-04-20 18:47:19 +0200

    init

---
+++ src/lib.rs
@@ -0,0 +1,27 @@
+use wasm_bindgen::prelude::wasm_bindgen;
+
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+#[wasm_bindgen]
+pub fn compute(rounds: u32) -> u32 {
+    let mut count: u32 = 0;
+    for _ in 0..rounds {
+        for _ in 0..rounds {
+            count += 1;
+        }
+    }
+    count
+}
+
+#[wasm_bindgen]
+pub fn say() -> String {
+    let r = String::from("hello from Rust!!!");
+    return r;
+}
+
+#[wasm_bindgen]
+pub fn echo(content: &str) -> String {
+    return content.to_string();
+}
---
+++ test.ts
@@ -0,0 +1,12 @@
+import init , { source, compute, say, echo } from "./wasm.js";
+
+await init(source);
+
+const testCompute = compute(42);
+console.log(testCompute);
+
+const testSay = say()
+console.log(testSay)
+
+const testEcho = echo("Hola Rust!!!");
+console.log(testEcho);
